956f08be6b8464c50164ef24d66e506b82204fd1
Sanjanaa1 <134793597+Sanjanaa1@users.noreply.github.com>
2025-04-10 14:58:17 +0530

    Create add.cob

---
+++ add.cob
@@ -0,0 +1,24 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. AddThreeNumbers.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 NUM1 PIC 9(3) VALUE 0.
+       01 NUM2 PIC 9(3) VALUE 0.
+       01 NUM3 PIC 9(3) VALUE 0.
+       01 RESULT PIC 9(4) VALUE 0.
+
+       PROCEDURE DIVISION.
+       MAIN-PARA.
+           DISPLAY "Enter first number: " WITH NO ADVANCING.
+           ACCEPT NUM1.
+           DISPLAY "Enter second number: " WITH NO ADVANCING.
+           ACCEPT NUM2.
+           DISPLAY "Enter third number: " WITH NO ADVANCING.
+           ACCEPT NUM3.
+
+           ADD NUM1 NUM2 NUM3 GIVING RESULT.
+
+           DISPLAY "The sum of the three numbers is: " RESULT.
+
+           STOP RUN.
